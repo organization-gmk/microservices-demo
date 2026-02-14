99c66e24f6a4b8511e52257b8635cf7e76ff1c43
Krishna <gmkrishna097@gmail.com>
2026-02-14 20:06:19 +0000

    fixing eks module

--- Aws-infra/iac/modules/eks/outputs.tf
+++ Aws-infra/iac/modules/eks/outputs.tf
@@ -1,9 +1,24 @@
 output "cluster_name" {
-  description = "EKS cluster ID"
-  value       = aws_eks_cluster.gmk-cluster.id
+  description = "EKS cluster name"
+  value       = aws_eks_cluster.gmk_cluster.name
 }
 
 output "cluster_arn" {
   description = "EKS cluster ARN"
-  value       = aws_eks_cluster.gmk-cluster.arn
+  value       = aws_eks_cluster.gmk_cluster.arn
+}
+
+output "cluster_endpoint" {
+  description = "EKS cluster endpoint"
+  value       = aws_eks_cluster.gmk_cluster.endpoint
+}
+
+output "cluster_certificate_authority_data" {
+  description = "EKS cluster certificate authority data"
+  value       = aws_eks_cluster.gmk_cluster.certificate_authority[0].data
+}
+
+output "cluster_oidc_issuer_url" {
+  description = "The URL on the EKS cluster OIDC Issuer"
+  value       = aws_eks_cluster.gmk_cluster.identity[0].oidc[0].issuer
 }
--- Aws-infra/iac/providers.tf
+++ Aws-infra/iac/providers.tf
@@ -4,21 +4,10 @@ provider "aws" {
 
 
 
-data "aws_eks_cluster" "gmk-cluster" {
-  name = local.cluster_name
-  depends_on = [module.eks]
-}
-
-data "aws_eks_cluster_auth" "gmk-cluster" {
-  name = local.cluster_name
-  depends_on = [module.eks]
-}
-
 provider "kubernetes" {
-  host                   = data.aws_eks_cluster.gmk-cluster.endpoint
-  cluster_ca_certificate = base64decode(data.aws_eks_cluster.gmk-cluster.certificate_authority[0].data)
-  token                  = data.aws_eks_cluster_auth.gmk-cluster.token
-
+  host                   = module.eks.cluster_endpoint
+  cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
+  
   exec {
       api_version = "client.authentication.k8s.io/v1beta1"
       command     = "aws"
@@ -26,7 +15,8 @@ provider "kubernetes" {
         "eks",
         "get-token",
         "--cluster-name",
-        data.aws_eks_cluster.gmk-cluster.name
+        
+        module.eks.cluster_name
       ]
     }
 }
