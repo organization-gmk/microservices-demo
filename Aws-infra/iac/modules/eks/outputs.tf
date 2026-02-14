40a70ab30b864560747c35ff7df22ff3d340cc44
Krishna <gmkrishna097@gmail.com>
2026-02-14 20:41:02 +0000

    fixing eks module

--- Aws-infra/iac/modules/eks/outputs.tf
+++ Aws-infra/iac/modules/eks/outputs.tf
@@ -15,10 +15,10 @@ output "cluster_endpoint" {
 
 output "cluster_certificate_authority_data" {
   description = "EKS cluster certificate authority data"
-  value       = aws_eks_cluster.gmk_cluster.certificate_authority.data
+  value       = aws_eks_cluster.gmk_cluster.certificate_authority[0].data
 }
 
 output "cluster_oidc_issuer_url" {
   description = "The URL on the EKS cluster OIDC Issuer"
-  value       = aws_eks_cluster.gmk_cluster.identity.oidc.issuer
+  value       = aws_eks_cluster.gmk_cluster.identity[0].oidc[0].issuer
 }
