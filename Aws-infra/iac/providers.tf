99a3e3323e6e141e815566ba728bdc4d306feb1f
Krishna <gmkrishna097@gmail.com>
2026-02-23 09:19:13 +0000

    fixing eks token issue while destroying service account

--- Aws-infra/iac/providers.tf
+++ Aws-infra/iac/providers.tf
@@ -6,27 +6,22 @@ data "aws_eks_cluster_auth" "cluster" {
   name = module.eks.cluster_name
 }
 
-# provider "kubernetes" {
-#   host                   = module.eks.cluster_endpoint
-#   cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
-  
-#   exec {
-#       api_version = "client.authentication.k8s.io/v1beta1"
-#       command     = "aws"
-#       args = [
-#         "eks",
-        
-#         "get-token",
-#         "--cluster-name",
-
-#         module.eks.cluster_name
-#       ]
-#     }
-# }
-
-
 provider "kubernetes" {
   host                   = module.eks.cluster_endpoint
   cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
-  token                  = data.aws_eks_cluster_auth.cluster.token
+  
+  exec {
+      api_version = "client.authentication.k8s.io/v1beta1"
+      command     = "aws"
+      args = [
+        "eks",
+        
+        "get-token",
+        "--cluster-name",
+
+        module.eks.cluster_name
+      ]
+    }
 }
+
+
